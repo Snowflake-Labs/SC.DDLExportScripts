051666fe51bc2c4d3e732882f4c308343f828d80
Orellabac <mauricio.rojas@mobilize.net>
2021-06-27 17:02:28 +0000

    integrating the Teradata Export scripts as a python module
    Former-commit-id: e5c0bc4cafcf94490b912546edece0db817acbe6
    Former-commit-id: 7f5e07705637f76711463d34fed8142f88bec720 [formerly e77a3ce866455de8f3116cd38120c6782138c139]
    Former-commit-id: beee3519195bcc5e95c9586f32ac792c7b804e73

---
+++ Teradata/teraexport/__init__.py
@@ -0,0 +1,7 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Mobilize.NET  All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+
+__version__ = '1.0.0a1'
