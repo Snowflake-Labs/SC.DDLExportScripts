121226e5c636009beec161d27cd1a4d0c26a9c6c
Orellabac <mauricio.rojas@mobilize.net>
2021-06-27 17:02:28 +0000

    integrating the Teradata Export scripts as a python module
    Former-commit-id: 080734fdb449699eb477fd363643c263d47de25d
    Former-commit-id: 7a8b26ace8b2dba67c73f65bc1774feda87385f6

---
+++ Teradata/split/__main__.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+import sys
+import re
+import os
+import json
+import argparse
+
+
+arguments_parser = argparse.ArgumentParser(description="DDLs file splitter for SnowConvert")
+arguments_parser.add_argument('--inputfile',required=True, help='This is the DDL file to split')
+arguments_parser.add_argument('--outdir', required=True, help='This is the directory where the splitted files will be put')
+arguments_parser.add_argument('--duplicates', help='If given duplicate files will be stored on this directory. NOTE: do not put this directory in the the same output directory, this way when running SnowConvert you can just point it to the directory where the splitted files are')
+arguments = arguments_parser.parse_args()
+
+input_file = arguments.inputfile
+base_output_dir = arguments.outdir
+
+
+extracted_counts = {}
+elements = []
+
+def element_already_extracted(full_object_name):
+    if full_object_name in extracted_counts.keys():
+        return True
+    else:
+        return False
+
+def update_extraction_count(full_object_name): 
+    previous_count = 0
+    if full_object_name in extracted_counts.keys():
+        previous_count = extracted_counts[full_object_name]
+    extracted_counts[full_object_name] = previous_count + 1
+
+def get_proper_target_directory(kind, schema, duplicates = False):
+    if (kind=="table" or kind == "view" or kind == "procedure" or kind == "macro" or kind == "function" or kind == "joinindex" or kind == "insert"):
+        if (schema is not None):
+            if duplicates:
+                return os.path.join(arguments.duplicates, kind, schema)
+            else:
+                return os.path.join(base_output_dir, kind, schema)
+    if (kind == "schema"):
+        if duplicates:
+            return os.path.join(arguments.duplicates, kind)
+        else:
+            return os.path.join(base_output_dir, kind)
+    if duplicates:
+        return arguments.duplicates
+    else:
+        return base_output_dir
+
+def get_proper_extension(kind):
+    if (kind == "view"):
+        return ".sql"
+    if (kind == "table"):
+        return ".sql"
+    if (kind == "macro"):
+        return ".sql"
+    if (kind == "procedure"):
+        return ".sql"
+    if (kind == "function"):
+        return ".sql"
+    if (kind == "schema"):
+        return ".sql"
+    if (kind == "joinindex"):
+        return ".sql"
+    if (kind == "insert"):
+        return ".sql"        
+    return ".unknown"
+
+
+def process_file(input_file):
+    try:
+        f = open(input_file, "r")
+        s = f.read()
+        f.close()
+    except UnicodeDecodeError:
+        try:
+            f = open(input_file, "r", encoding="ISO-8859-1")
+            s = f.read()
+            f.close()
+        except Exception:
+            print("Error opening file [" + input_file + "] please review encoding and file permissions")
+            s = ""
+    p = s.replace("/* <sc-","/* <cconv> *//* <sc-")
+    delimiter = "/* <cconv> */"
+    stmnts = p.split(delimiter)
+
+    if len(stmnts) > 0:
+        i = 1
+        while i < len(stmnts):
+            sp = stmnts[i]
+            matches = re.search("<sc-(.*)>(.*)\<.sc-(.*)\>",sp)
+            if (matches is not None):
+                groups = matches.groups()
+                kind = groups[0].lower()
+                full_object_name = groups[1].lower().strip()
+                object_name = full_object_name
+                schema = None
+                if ("." in full_object_name):
+                    parts = full_object_name.split(".")
+                    schema = parts[0]
+                    object_name = parts[1]
+                element_info = {"full_object_name": full_object_name, "object_name":object_name, "schema":schema, "type": kind, "source": input_file, "position": i}
+                elements.append(element_info)
+                print("Extracting " +  full_object_name)
+                # We need to make sure the target dir exists
+                target_dir = get_proper_target_directory(kind, schema)
+                if (not os.path.exists(target_dir)):
+                    os.makedirs(target_dir)
+                target_filename = os.path.join(target_dir,object_name + get_proper_extension(kind))
+                is_duplicate = False
+                if element_already_extracted(full_object_name):
+                    is_duplicate = True
+                    print("    >>  Duplicate found for " + full_object_name)
+                    target_dir = get_proper_target_directory(kind, schema, True)
+                    target_filename = os.path.join(target_dir,object_name + "_" + str(extracted_counts[full_object_name]) + get_proper_extension(kind))
+                    duplicate_dir = os.path.dirname(target_filename)
+                    # check if duplicates dir was given
+                    if arguments.duplicates:
+                        if (not os.path.exists(duplicate_dir)):
+                            os.makedirs(duplicate_dir)
+                update_extraction_count(full_object_name)
+                if is_duplicate and not arguments.duplicates:
+                    pass
+                else:
+                    f = open(target_filename,"w+")
+                    f.write(sp)
+                    f.close()
+            i += 1
+        
+
+
+print("Processing file " +  input_file)
+process_file(input_file)
+print("Done")
---
+++ Teradata/teraexport/__main__.py
@@ -0,0 +1,105 @@
+import teraexport
+import argparse
+import sys
+import os
+import stat
+
+args = sys.argv[1:]
+parser = argparse.ArgumentParser(
+        prog=u'sc-tera-export',
+        description=u'Mobilize.NET Teradata Code Export Tools' +
+        'Version {}'.format(teraexport.__version__))
+
+parser.add_argument(
+        u'-S', u'--server',
+        dest=u'Server',
+        required=True,
+        metavar=u'',
+        help=u'Server address. For example: 127.0.0.1')
+
+parser.add_argument(
+        u'-U', u'--user',
+        dest=u'UserId',
+        metavar=u'',
+        required=True,
+        help=u'Login ID for server. Usually it will be the DBC user')
+
+parser.add_argument(
+        u'-P', u'--password',
+        dest=u'Password',
+        required=True,
+        metavar=u'',
+        help=u'The password for the given user.') 
+
+parameters = parser.parse_args(args)
+
+
+print("This tool will generate some bash shell and BTEQ scripts in the current folder ")
+print("that will be used to export the Teradata Database Code that will be upgraded to Snowflake.")
+answer = input("Please confirm to continue [y/n]?")
+answer = answer.lower()
+
+if answer and answer.startswith("y"):
+    print("continue")
+else:
+    print()
+    print("Bye. Teradata Export for SnowConvert Terminated")
+    exit(1)
+
+## Get python module directory
+teraexportdir = os.path.dirname(os.path.realpath(__file__))
+
+bindir =  os.path.realpath(os.path.join(teraexportdir,"bin"))
+scriptsdir =  os.path.realpath(os.path.join(teraexportdir,"scripts"))
+
+# current dir 
+cwd = os.getcwd()
+
+# Create Target Dirs
+target_bindir = os.path.join(cwd, "bin")
+target_scriptdir = os.path.join(cwd,"scripts")
+
+try:
+        os.mkdir(target_bindir)
+except FileExistsError:
+        #ignore error
+        pass
+try:
+        os.mkdir(target_scriptdir)
+except:
+        FileExistsError
+        #ignore error
+        pass        
+
+## read create_ddls
+with open(os.path.join(bindir,"create_ddls.sh"),"r") as f:
+    contents = f.read()
+    contents = contents.replace("@@VERSION",teraexport.__version__)
+    contents = contents.replace("@@SERVER",parameters.Server)
+    contents = contents.replace("@@USER",parameters.UserId)
+    contents = contents.replace("@@PASSWORD",parameters.Password)
+    with open(os.path.join(target_bindir,"create_ddls.sh"),"w+") as fout:
+        fout.write(contents)
+
+# copy create_load_to_sf
+os.system("cp " + os.path.join(bindir,"create_load_to_sf.sh") + " " + os.path.join(target_bindir, "create_load_to_sf.sh"))
+
+# make the scripts executable
+os.chmod(os.path.join(target_bindir, "create_ddls.sh"),(
+        stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | 
+        stat.S_IXGRP | stat.S_IXUSR | stat.S_IXOTH))
+os.chmod(os.path.join(target_bindir, "create_load_to_sf.sh"),(
+        stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | 
+        stat.S_IXGRP | stat.S_IXUSR | stat.S_IXOTH))
+
+# copy all bteq scripts
+os.system("cp -rf " + scriptsdir + " " + target_scriptdir)
+
+# open the script on an editor so the user can customize it
+editor = os.getenv('EDITOR') or "vi"
+if editor:
+    os.system(editor + ' ' + os.path.join(target_bindir,"create_ddls.sh"))
+
+os.chdir(target_bindir)    
+print("You can now execute the generated 'create_ddls.sh' script.")
+print("Before running review it to make sure that the databases you want to include/exclude are properly set")    
