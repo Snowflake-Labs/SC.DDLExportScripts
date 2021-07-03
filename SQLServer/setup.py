40f9a3868f2e19bd338e62b318215fd63f9d8368
Orellabac <mauricio.rojas@mobilize.net>
2021-07-03 20:54:10 +0000

    some minor changes in workflow
    Former-commit-id: 2ffa63abb7c0507eb4180f37b3f7eaa5d521a4f9 [formerly 9b25878e1674f784e7f06173e1287a438e365485]
    Former-commit-id: e6b6973c96281b395245c6a9dec59666d964a81d [formerly 39f4dc4507adf8084fda43c26dac13e7172b472d]
    Former-commit-id: 3f06811d5bb78049f3c56716d22f6aa54ad0aca6

--- SQLServer/setup.py
+++ SQLServer/setup.py
@@ -23,7 +23,7 @@ if system == 'Windows':
 if system == 'Darwin':
     arrange_tool = 'bin/mac64/ExtractionCleanUp'
 
-SQL_EXPORT_VERSION = '0.0.4'
+SQL_EXPORT_VERSION = '0.0.5'
 
 CLASSIFIERS = [
     'Development Status :: 3 - Alpha',
