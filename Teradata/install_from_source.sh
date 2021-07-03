7a9734856777bac7e978939cb9a4e295ac16c0e0
Orellabac <mauricio.rojas@mobilize.net>
2021-07-03 13:54:46 +0000

    removing some comments
    Former-commit-id: c87ba7dc0b6bea770b3bb2485e5dad970ed49afc [formerly 9a2fccc1376e9403afda8ed24681e66e702c79bc]
    Former-commit-id: f2227a3525c3f4ebe61629784f1d9e1849c0890a [formerly 07590bd6de1f89f30d785b289575610a71758da1]
    Former-commit-id: 8ee68ae17e5fa17f51b54ceedad75d5bde04d629

--- Teradata/install_from_source.sh
+++ Teradata/install_from_source.sh
@@ -2,19 +2,10 @@ REPO=https://test.pypi.org
 FILES_REPO=https://test-files.pythonhosted.org
 pythonPackage=snowconvert-export-tera;curl -Ls $REPO/pypi/$pythonPackage/json > snowconvert-export-tera.json
 LATEST=$(cat snowconvert-export-tera.json | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["info"]["version"])');
-echo "LATEST VERSION IS $LATEST"
+echo "DOWNLOADING LATEST VERSION: $LATEST"
 echo "download $FILES_REPO/packages/source/s/snowconvert-export-tera/snowconvert-export-tera-$LATEST.tar.gz"
 rm -f snowconvert-expor-tera*
 wget $FILES_REPO/packages/source/s/snowconvert-export-tera/snowconvert-export-tera-$LATEST.tar.gz
 tar -xzf snowconvert-export-tera-$LATEST.tar.gz
 cd snowconvert-export-tera-$LATEST
 python setup.py install
-#https://test-files.pythonhosted.org/packages/source/s/snowconvert-export-tera/snowconvert-export-tera-0.0.3.tar.gz;
-## https://pypi.io/packages/source/{ package_name_first_letter }/{ package_name }/{ package_name }-{ package_version }.tar.gz
-#https://test.pypi.org/packages/source/s/snowconvert-export-tera/snowconvert-export-tera-0.0.1a4.tar.gz
-#https://test.pypi.org/packages/source/s/snowconvert-export-tera/snowconvert-export-tera-0.0.1a4.tar.gz
-
-#https://pypi.io/packages/source/p/pip/pip-19.3.1.tar.gz
-#https://pypi.org/packages/source/p/pip/pip-19.3.1.tar.gz
-#https://files.pythonhosted.org/packages/source/p/pip/pip-19.3.1.tar.gz
-#https://files.pythonhosted.org/packages/ce/ea/9b445176a65ae4ba22dce1d93e4b5fe182f953df71a145f557cffaffc1bf/pip-19.3.1.tar.gz
