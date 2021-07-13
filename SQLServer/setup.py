2b88c1f7c5e54b06e672e95f3b062a007a712377
Orellabac <mauricio.rojas@mobilize.net>
2021-07-13 05:34:19 +0000

    correct script path
    Former-commit-id: 6ab22f0bb89cc9621b067bcc4cb62a60cd69bcd0

--- SQLServer/setup.py
+++ SQLServer/setup.py
@@ -23,7 +23,7 @@ if system == 'Windows':
 if system == 'Darwin':
     arrange_tool = 'bin/mac64/ExtractionCleanUp'
 
-SQL_EXPORT_VERSION = '0.0.9'
+SQL_EXPORT_VERSION = '0.0.10'
 
 CLASSIFIERS = [
     'Development Status :: 3 - Alpha',
--- Teradata/setup.py
+++ Teradata/setup.py
@@ -12,7 +12,7 @@ import sys
 
 from setuptools import setup
 
-TERA_EXPORT_VERSION='0.0.9'
+TERA_EXPORT_VERSION='0.0.10'
 
 CLASSIFIERS = [
     'Development Status :: 3 - Alpha',
