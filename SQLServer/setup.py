b5ee3b5754e499783587db78055c1e9e648eb203
Orellabac <orellabac@gmail.com>
2021-07-20 23:07:05 +0000

    Fixing issue running on Windows 10

--- SQLServer/setup.py
+++ SQLServer/setup.py
@@ -23,7 +23,7 @@ if system == 'Windows':
 if system == 'Darwin':
     arrange_tool = 'bin/mac64/ExtractionCleanUp'
 
-SQL_EXPORT_VERSION = '0.0.14'
+SQL_EXPORT_VERSION = '0.0.15'
 
 CLASSIFIERS = [
     'Development Status :: 3 - Alpha',
