24c7c0e7a84a598c532ef2204a15282077853189
Orellabac <orellabac@gmail.com>
2021-11-07 08:11:19 -0600

    Adding Scripts for DB2 (#4)
    * initial version
    
    * update scripts file
    
    * update export script and usage
    
    * Add link into main README
    
    * update readme
    
    * some final updates on the scripts

---
+++ DB2/bin/create_ddls.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+echo "DB2 DDL Export script"
+echo "Getting list of databases"
+OUTPUTDIR="../object_extracts"
+### Get List of Database
+
+## You can modify this variable to exclude some databases:
+## For example if you want to exclude database TESTDB just set:
+## DATABASES_TO_EXCLUDE="TESTDB"
+## If you want to exclude database TESTDB and database SAMPLE just set:
+## DATABASES_TO_EXCLUDE="TESTDB|SAMPLE"
+## You can use regular any valid regular expression as a pattern to exclude the databases to exclude
+DATABASES_TO_EXCLUDE="XXXXXXX"
+
+## DB Reports
+SCHEMA_FILTER="%"
+
+DDLS="$OUTPUTDIR/DDL"
+REPORTS="$OUTPUTDIR/Reports"
+mkdir -p $DDLS
+mkdir -p $REPORTS
+DBS=$( db2 list db directory | grep Indirect -B 5 |grep "Database alias" |awk {'print $4'} |sort -u | uniq 2>/dev/null | grep -v -E $DATABASES_TO_EXCLUDE)
+for db in $DBS
+do
+    mkdir -p "$DDLS/$db"
+    mkdir -p "$REPORTS/$db"
+    echo "Processing Database $db"
+    db2look -d $db -e -l > "$DDLS/$db/DDL_All.sql"
+
+    ## Get REPORTS
+    ## Get table volumetrics
+    db2 "connect to $db"
+
+    db2 "SELECT SUBSTR(TABSCHEMA,1,10) AS SCHEMA,  SUBSTR(TABNAME,1,15) AS TABNAME,
+    INT(DATA_OBJECT_P_SIZE) AS OBJ_SZ_KB,
+    INT(INDEX_OBJECT_P_SIZE) AS INX_SZ_KB,
+    INT(XML_OBJECT_P_SIZE) AS XML_SZ_KB
+    FROM  SYSIBMADM.ADMINTABINFO
+    WHERE TABSCHEMA LIKE '%'
+    ORDER BY 3 DESC;" > "$REPORTS/$db/volumetrics_per_object.txt"
+
+    db2 "SELECT SUBSTR(TABSCHEMA,1,10) AS SCHEMA,  
+    SUM(DATA_OBJECT_P_SIZE) AS OBJ_SZ_KB,
+    SUM(INDEX_OBJECT_P_SIZE) AS INX_SZ_KB,
+    SUM(XML_OBJECT_P_SIZE) AS XML_SZ_KB
+    FROM    SYSIBMADM.ADMINTABINFO
+    GROUP BY TABSCHEMA
+    ORDER BY 2 DESC;"  > "$REPORTS/$db/volumetrics_per_database.txt"
+
+### DATABASE SIZE
+
+    db2 "CALL GET_DBSIZE_INFO(?,?,?,-1)" > "$REPORTS/$db/db_size.txt"
+
+done
+
+
+
+
