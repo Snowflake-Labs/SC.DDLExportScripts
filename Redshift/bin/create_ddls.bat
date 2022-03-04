f10be5e498f8c588065ddca5b10ab429de275e90
jchaconbogarin-mb <93597058+jchaconbogarin-mb@users.noreply.github.com>
2022-03-03 22:12:52 -0600

    Added logging, better exception handling and multithreading (#26)

--- Redshift/bin/create_ddls.bat
+++ Redshift/bin/create_ddls.bat
@@ -5,7 +5,7 @@ REM                       below to match your environment. Once completed, your 
 REM ---- Variables to change ----
 
 REM General Variables
-SET OUTPUT_PATH="<output_path>"
+SET OUTPUT_PATH="<C:\example\path>"
 
 REM AWS RedShift Variables
 SET RS_CLUSTER="<redshift_cluster_identifier>"
@@ -15,6 +15,7 @@ SET RS_SECRET_ARN="<secret_arn>"
 REM Script Variables
 SET SCHEMA_FILTER="lower(schemaname) LIKE '%%'"
 SET BATCH_WAIT="0.2"
+SET THREADS="4"
 REM ---- END: Variables to change ----
 
 mkdir %OUTPUT_PATH%
@@ -25,4 +26,4 @@ mkdir %OUTPUT_PATH%\object_extracts\DDL
 REM mkdir %OUTPUT_PATH%\object_extracts\Reports
 REM mkdir %OUTPUT_PATH%\object_extracts\Storage
 
-python ../scripts/_ddl_extractor.py %RS_CLUSTER% %RS_DATABASE% %RS_SECRET_ARN% %OUTPUT_PATH% %SCHEMA_FILTER% %BATCH_WAIT%
+python ../scripts/_ddl_extractor.py --rs-cluster %RS_CLUSTER% --rs-database %RS_DATABASE% --rs-secret-arn %RS_SECRET_ARN% --output-path %OUTPUT_PATH% --schema-filter %SCHEMA_FILTER% --batch-wait %BATCH_WAIT% --threads %THREADS%
--- Redshift/scripts/_ddl_extractor.py
+++ Redshift/scripts/_ddl_extractor.py
@@ -1,8 +1,16 @@
-from _boto3_client import get_client
+import argparse
+import datetime as datetime
+import logging
 import os
 import re
 import sys
 import time
+
+from _boto3_client import get_client
+from itertools import repeat
+from multiprocessing import cpu_count
+from multiprocessing.pool import ThreadPool as Pool
+
 
 '''
 ===========================
@@ -22,12 +30,10 @@ class Query:
         self.code = []
         self.out_path = out_path
 
-
     def is_query_valid(self, client):
         resp = client.describe_statement(Id=self.query_id)
         if 'Error' in resp:
             raise RuntimeError(resp['Error'])
-            pass
         else:
             return resp['ResultRows'] >= 0
 
@@ -42,10 +48,9 @@ class Query:
 
     def save_code(self):
         filename = self.get_output_path()
-        print(f'Saving {self.object_type} DDL code to {filename}')
+        print_and_log(f'Saving {self.object_type} DDL code to {filename}', logging.info)
         with open(filename, 'w') as f:
             f.writelines(self.code)
-
 
     def get_output_path(self):
         object_type_title = self.object_type.title()
@@ -114,10 +119,10 @@ class ProcedureQuery(Query):
                 queries.pop(0)  # First element is always empty
                 self.procedures_processed = len(queries)
                 i = 0
-                batch = 0
-                batches = abs(self.procedures_processed / 40)
-                while i < self.procedures_processed:
-                    print(f'Sending batch {batch}/{batches}')
+                batch = 1
+                batches = int(self.procedures_processed / 40) + 1
+                while i <= self.procedures_processed:
+                    print(f'Executing batch {batch}/{batches}')
                     edge = i + 40 if i + 40 < self.procedures_processed else self.procedures_processed
                     batch_statement_response = client.batch_execute_statement(
                         ClusterIdentifier=RS_CLUSTER
@@ -133,28 +138,38 @@ class ProcedureQuery(Query):
                         time.sleep(BATCH_WAIT)
                 return True
             else:
-                # Log no procedures where found
+                print_and_log('No procedures were found in the database and schema sepecified', logging.info)
                 return True
         else:
             return False
 
-    def save_code(self, client):
-        print(f'Found {self.procedures_processed} procedures')
-        i = 0
-        for proc in self.finished_statements:
-            if i % 10 == 0:
-                print(f'Getting code for procedure {i}/{self.procedures_processed}')
-            i += 1
-            query_result = client.get_statement_result(Id=proc['id'])
+    @staticmethod
+    def get_procedure_code(proc, client):
+        query_id = proc['id']
+        proc_name = proc['object_name']
+        try:
+            query_result = client.get_statement_result(Id=query_id)
             if 'Records' in query_result:
                 proc_code = query_result['Records'][0][0]['stringValue']
-                proc_name = proc['object_name']
-                self.code.append(f'/* <sc-procedure> {proc_name} </sc-procedure> */\n\n{proc_code}\n')
+                return f'/* <sc-procedure> {proc_name} </sc-procedure> */\n\n{proc_code}\n'
+        except Exception as ex:
+            msg = f'Unable to get code for procedure {proc_name}.'
+            print(f'{msg} Please check the logs for more details.')
+            logging.exception(f'{msg} Query ID: {query_id}. Exception details:')
+            return ''
+
+    def save_code(self, client):
+        print_and_log(f'Found {self.procedures_processed} procedures', logging.info)
+
+        with Pool(4) as p:
+            self.code = p.starmap(ProcedureQuery.get_procedure_code, zip(self.finished_statements, repeat(client)))
+
+        print_and_log('Finished retrieving procedures.', logging.info)
+
         filename = self.get_output_path()
-        print(f'Saving {self.object_type} DDL code to {filename}')
+        print_and_log(f'Saving {self.object_type} DDL code to {filename}', logging.info)
         with open(filename, 'w') as f:
             f.writelines(self.code)
-        return True
 
 
 def read_ddl_queries():
@@ -185,6 +200,10 @@ def execute_ddl_queries():
         q.query_id = resp['Id']
 
 
+def print_and_log(msg, level):
+    print(msg)
+    level(msg)
+
 '''
 =================================
 == Global variables definition ==
@@ -194,42 +213,75 @@ abspath = os.path.abspath(__file__)
 dir_name = os.path.dirname(abspath)
 os.chdir(dir_name)
 
-args = sys.argv
+parser = argparse.ArgumentParser()
+parser.add_argument('--rs-cluster',  help='Redshift Cluster Identifier')
+parser.add_argument('--rs-database', help='Redshift Database Name')
+parser.add_argument('--rs-secret-arn', help='SecretsManager secret ARN. This secret should contain the credentials to connect to the Redshift Cluster.')
+parser.add_argument('--output-path', help='Output path where the code will be exported.')
+parser.add_argument('--schema-filter', help='SQL statement to filter the schema.')
+parser.add_argument('--batch-wait', default=0.2, help='When sending the queries for the procedures, this will be the time to wait (in seconds) between group of batches (every 200 queries)', type=float)
+parser.add_argument('--threads', default=4, help='When extracting the code for the procedures, the amount of threads to use to make the queries to AWS Redshift-Data API. This will decrease the extraction times considerably.', type=int)
+args = parser.parse_args()
 
-RS_CLUSTER = args[1]
-RS_DATABASE = args[2]
-RS_SECRET_ARN = args[3]
-OUT_PATH = args[4]
-SCHEMA_FILTER = args[5]
-BATCH_WAIT = float(args[6])
+RS_CLUSTER = args.rs_cluster
+RS_DATABASE = args.rs_database
+RS_SECRET_ARN = args.rs_secret_arn
+OUT_PATH = args.output_path
+SCHEMA_FILTER = args.schema_filter
+BATCH_WAIT = args.batch_wait
+THREADS = args.threads
 
-RS_CLIENT = get_client()
+current_date = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+log_path = os.path.join(OUT_PATH, 'log', f'{current_date}.log')
+logging.basicConfig(filename=log_path, encoding='utf-8', level=logging.INFO)
 
-QUERIES = read_ddl_queries()
-execute_ddl_queries()
-proc_query = None
+cpu_count = cpu_count()
+if cpu_count < THREADS:
+    print_and_log(f'Amount of threads specified exceeds maximum of current CPU. Current CPU count is {cpu_count}.', logging.error)
+    exit(1)
+
+try:
+    import boto3
+    import botocore.exceptions
+
+    RS_CLIENT = get_client()
+    QUERIES = read_ddl_queries()
+    execute_ddl_queries()
+    proc_query = None
+    if len(QUERIES) == 0:
+        print_and_log('No ddl queries detected. Please make sure to copy the complete repository folder.', logging.error)
+        exit(1)
+except ModuleNotFoundError:
+    print_and_log('Boto3 library not installed. Please `run pip install boto3` for the script to work.', logging.error)
+    exit(1)
+except botocore.exceptions.NoCredentialsError:
+    print_and_log('Unable to initialize connection to s3 with the credentials provided. Make sure your credentials are properly configured', logging.error)
+    exit(1)
 
 for q in QUERIES:
-    i = 0
-    print(f'Validating query result for {q.object_type}')
+    i = 1
+    print_and_log(f'Validating query result for {q.object_type}', logging.info)
+
+    if isinstance(q, ProcedureQuery):
+        proc_query = q
+
     while i <= 60:
-        print(f'>>> Query result validation number {i}/60')
+        print(f'>>> Query result retrieval attempt {i}/60')
         try:
             if q.get_code(RS_CLIENT):
+                if not isinstance(q, ProcedureQuery):
+                    q.save_code()
                 break
         except RuntimeError as e:
-            print(f'Failed to extract data for {q.object_type}. Failed query with error message:')
-            print(e)
+            print(f'WARNING: Failed to extract data for {q.object_type}. Please check the logs for full error message.')
+            logging.exception(e)
             break
         time.sleep(5)
         i += 1
-    if not isinstance(q, ProcedureQuery):
-        q.save_code()
-    else:
-        proc_query = q
+
 
 if proc_query is not None:
-    print('Validating individual queries for procedures')
+    print_and_log('Validating individual queries for procedures', logging.info)
     elapsed_validation = None
     elapsed_saving = None
     while i < 60:
@@ -237,7 +289,7 @@ if proc_query is not None:
         if proc_query.validate_procedure_queries(RS_CLIENT):
             end = time.time()
             elapsed_validation = end - start
-            print('Writing Extracted Procedure DDL code')
+            print_and_log('Writing Extracted Procedure DDL code', logging.info)
             start = time.time()
             proc_query.save_code(RS_CLIENT)
             end = time.time()
@@ -246,8 +298,8 @@ if proc_query is not None:
         time.sleep(5)
         i += 1
 
-    print(f'Procedure validation elapsed seconds: {elapsed_validation}')
-    print(f'Procedure extraction elapsed seconds: {elapsed_saving}')
+    print_and_log(f'Procedure validation elapsed seconds: {elapsed_validation}', logging.info)
+    print_and_log(f'Procedure extraction elapsed seconds: {elapsed_saving}', logging.info)
 
 if __name__ == 'main':
     pass
