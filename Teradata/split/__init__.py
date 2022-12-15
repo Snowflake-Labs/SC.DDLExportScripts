a02f5750d1a42b9283b4bb3f968117049130b555
Orellabac <orellabac@gmail.com>
2022-12-15 12:22:06 -0600

    Removing unneeded files from Teradata scripts (#42)

--- Teradata/split/__init__.py
+++
@@ -1,7 +0,0 @@
-# --------------------------------------------------------------------------------------------
-# Copyright (c) Mobilize.NET  All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
-# --------------------------------------------------------------------------------------------
-
-
-__version__ = '0.0.10'
